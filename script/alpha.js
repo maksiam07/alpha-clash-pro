92e2e293e95eb14396d7dd7cc0df5044f842c406
maksiam07 <saklinearafat07@gmail.com>
2024-02-20 05:21:39 +0800

    score update

--- script/alpha.js
+++ script/alpha.js
@@ -8,10 +8,35 @@
 //     playGroundSection.classList.remove('hidden');
 //     // console.log(playGroundSection.classList)
 // }
+
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key
+    console.log('player pressed', playerPressed )
+    // get the expected key to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point')
+        console.log('you have correctly pressed', expectedAlphabet)
+        removeBackGroundColorById(expectedAlphabet)
+        continueGame();
+    }
+    else{
+        console.log('you missed. you lost a life')
+    }
+
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+
 function continueGame(){
     // step 1 generate a random alphabet
     const alphabet=getARandomAlphabet();
-    console.log('your random alphabet', alphabet)
+    // console.log('your random alphabet', alphabet)
 
     // set randomly generated alphabet to the screen(show it)
     const currentAlphabetElement = document.getElementById('current-alphabet')
